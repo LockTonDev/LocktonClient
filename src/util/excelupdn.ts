8dc30bb72e6aa9604e5b952e99c0e58268822557
seonghyunwon <wsh1010@castis.com>
2024-01-31 18:02:29 +0900

    관세사 엑셀 다운시 합동/법인에 대한 버그 수정

--- src/util/excelupdn.ts
+++ src/util/excelupdn.ts
@@ -1773,7 +1773,7 @@ function mapperRow_CAA_JNT(excelMapper: object, excelDataRow: any) {
       subRow[excelMapper.소급담보일] = insuranceDTO?.cbr_data[i]?.insr_retr_dt;
       subRow[excelMapper.할인할증기준] = insuranceDTO?.cbr_data[i]?.insr_sale_year;
       subRow[excelMapper.할인할증] = insuranceDTO?.cbr_data[i]?.insr_sale_rt;
-      subRow[excelMapper.보험료] = insuranceDTO?.cbr_data[i]?.insr_amt;
+      subRow[excelMapper.개인보험료] = insuranceDTO?.cbr_data[i]?.insr_amt;
     }
 
     if (insuranceDTO.trx_data[i + 1]) {
@@ -1977,7 +1977,7 @@ function mapperRow_CAA_COR(excelMapper: object, excelDataRow: any) {
       subRow[excelMapper.소급담보일] = insuranceDTO?.cbr_data[i]?.insr_retr_dt;
       subRow[excelMapper.할인할증기준] = insuranceDTO?.cbr_data[i]?.insr_sale_year;
       subRow[excelMapper.할인할증] = insuranceDTO?.cbr_data[i]?.insr_sale_rt;
-      subRow[excelMapper.보험료] = insuranceDTO?.cbr_data[i]?.insr_amt;
+      subRow[excelMapper.개인보험료] = insuranceDTO?.cbr_data[i]?.insr_amt;
     }
 
     if (insuranceDTO.trx_data[i + 1]) {
