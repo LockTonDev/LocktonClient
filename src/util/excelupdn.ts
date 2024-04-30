5daa3622cab5f7e62de89e2876036f7c419368ff
shyun290 <shyun@gmail.com>
2024-04-30 15:04:08 +0900

    법무사 수정

--- src/util/excelupdn.ts
+++ src/util/excelupdn.ts
@@ -2693,7 +2693,7 @@ function mapperRow_LAW_JNT(excelMapper: object, excelDataRow: any) {
   row[excelMapper.소급담보일] = insuranceDTO.insr_retr_dt;
   row[excelMapper.사업자번호] = insuranceDTO.corp_cnno;
   row[excelMapper.법인번호] = insuranceDTO.corp_bnno;
-  row[excelMapper.보상한도] = insuranceDTO.insr_clm_lt_amt?.getValueBySplit(1);
+  row[excelMapper.보상한도] = insuranceDTO.insr_clm_lt_amt;
   row[excelMapper.보상한도_총한도] = insuranceDTO.insr_year_clm_lt_amt;
   row[excelMapper.자기부담금] = insuranceDTO.insr_psnl_brdn_amt?.getValueBySplit(1);
   row[excelMapper.기준보험료] = insuranceDTO.insr_base_amt;
