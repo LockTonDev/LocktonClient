1496b65a0021bc67f3554893e9bcaad7b6a313e7
shyun <shyun@gmail.com>
2024-08-05 18:18:55 +0900

    예외 조건 추가

--- src/views/contract/TAX/V_TTAX0030A10.vue
+++ src/views/contract/TAX/V_TTAX0030A10.vue
@@ -287,8 +287,10 @@
       }
       console.log(InsuranceList.value[0].cbr_data)
       //과거 이력 해지자 상태 변경
-      let cbrInfo = InsuranceList.value[0].cbr_data?.filter(item =>(_AUTH_USER.value.userNm == item.cbr_nm))
-      if(cbrInfo != '80' || cbrInfo != '90' ) InsuranceList.value[0].status_cd = cbrInfo.status_cd
+      if(InsuranceList.value.length > 0 && InsuranceList.value[0]?.cbr_data?.length>0) {
+        let cbrInfo = InsuranceList.value[0].cbr_data?.filter(item => (_AUTH_USER.value.userNm == item.cbr_nm))
+        if (cbrInfo != '80' || cbrInfo != '90') InsuranceList.value[0].status_cd = cbrInfo.status_cd
+      }
 
       newInsrYN.value = resultData.data.newInsrYN[0].data;
       if(resultData.data.renewalInsrUUID.length > 0) {
